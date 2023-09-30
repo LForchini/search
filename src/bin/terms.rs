43c28822e9736cb4ca519f18710a7d68366d1f0f
LForchini <git@lforchini.com>
2023-09-30 13:47:42 +0100

    Add terms (terminal wikipedia)

---
+++ src/bin/terms.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+use thiserror::Error;
+use wikipedia::Wikipedia;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg()]
+    term: Vec<String>,
+}
+
+fn main() -> Result<(), TermsError> {
+    let args = Args::parse();
+
+    let term = args.term.join(" ");
+
+    let wiki: Wikipedia<wikipedia::http::default::Client> = Wikipedia::default();
+    let page = wiki.page_from_title(term);
+
+    let content = page.get_content()?;
+    let content = content.split('\n').next().ok_or(TermsError::ParseError)?;
+
+    println!("{}", content);
+
+    Ok(())
+}
+
+#[derive(Debug, Error)]
+enum TermsError {
+    #[error("Could not find article")]
+    NotFound,
+
+    #[error("Could not parse article")]
+    ParseError,
+}
+
+impl From<wikipedia::Error> for TermsError {
+    fn from(_: wikipedia::Error) -> Self {
+        Self::NotFound
+    }
+}
